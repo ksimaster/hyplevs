6f4d708db754ecfb922b4513854677938a9fbae7
ksimaster <ksimasterlord@gmail.com>
2022-11-09 21:02:11 +0300

    Create project

---
+++ Assets/Scripts/Barrier.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barrier : MonoBehaviour
+{
+
+    [SerializeField] GameObject _bricksEffectPrefab;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerModifier playerModifier = other.attachedRigidbody.GetComponent<PlayerModifier>();
+        if (playerModifier) {
+            playerModifier.HitBarrier();
+            Destroy(gameObject);
+            Instantiate(_bricksEffectPrefab, transform.position, transform.rotation);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Progress.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using TMPro;
+using UnityEngine;
+
+
+public class Progress : MonoBehaviour
+{
+
+    public int Coins;
+    public int Width;
+    public int Height;
+
+    public static Progress Instance;
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            transform.parent = null;
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
